c41ac9b6ed38f26d9c4887fb4974be046389679f
TheSamuraiBr <mmsnborges@gmail.com>
2023-04-13 14:34:49 -0300

    I maked the page not found and page loanding

--- src/templates/Home/Index.jsx
+++ src/templates/Home/Index.jsx
@@ -1,25 +1,44 @@
 import { MockBase } from '../Base/mock';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Base } from '../Base';
-import * as Styled from './styles';
 import { mapData } from '../../api/map-data';
+import { PageNotFound } from '../PageNotFound';
+import { Loading } from '../Loading';
+
 function Home() {
-  const dataRef = useRef(true);
+  const isMounted = useRef(true);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const load = async () => {
-      const apiData = await fetch(
-        'http://localhost:1337/api/pages/1?populate=deep',
-      );
-      const data = await apiData.json();
-      const json = await Object.values(data);
-      const { attributes } = await json[0];
-      const pageData = mapData([attributes]);
-      dataRef.current = false;
-      console.log(pageData);
+      try {
+        const apiData = await fetch(
+          'http://localhost:1337/api/pages/1?populate=deep',
+        );
+        const data = await apiData.json();
+        const json = await Object.values(data);
+        const { attributes } = await json[0];
+        const pageData = await mapData([attributes]);
+        //console.log(pageData);
+        await setData(pageData[0]);
+      } catch (er) {
+        setData(undefined);
+      }
     };
-    load();
+    if (isMounted.current === true) {
+      load();
+    }
+    return () => {
+      isMounted.current = false;
+    };
   }, []);
+
+  if (data === undefined) {
+    return <PageNotFound />;
+  }
+  if (data && !data.slug) {
+    return <Loading />;
+  }
 
   return <Base {...MockBase} />;
 }
